40cec35c8615e0cfdca2431f068d1a0b80243c88
Rhuan Hianc <rhuan.hianc@el.com.br>
2025-04-19 19:15:07 -0300

    ✨ add pacman color palettes and update aliases

--- packages/action/palettes.ts
+++ packages/action/palettes.ts
@@ -19,9 +19,22 @@ export const basePalettes: Record<
     colorDots: ["#161b22", "#01311f", "#034525", "#0f6d31", "#00c647"],
     colorSnake: "purple",
   },
+  "pacman-light": {
+    colorDotBorder: "#1b1f230a",
+    colorDots: ["#ebedf0", "#fee975", "#fedc56", "#ffcc00", "#ffaa00"],
+    colorEmpty: "#ebedf0",
+    colorSnake: "#FFCC00",
+  },
+  "pacman-dark": {
+    colorDotBorder: "#1b1f230a",
+    colorEmpty: "#161b22", 
+    colorDots: ["#161b22", "#3a3000", "#5e4c00", "#806600", "#ffcc00"],
+    colorSnake: "#FFCC00",
+  },
 };
 
 // aliases
 export const palettes = { ...basePalettes };
 palettes["github"] = palettes["github-light"];
 palettes["default"] = palettes["github"];
+palettes["pacman"] = palettes["pacman-light"];
